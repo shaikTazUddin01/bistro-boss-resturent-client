5751531944d152db942e9f30f14bc8fe9f29d453
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-04-04 18:17:21 +0600

    added all menu section

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -6,6 +6,9 @@ import MenuSection from '../../Component/Menu/MenuSection';
 // images
 import bannerImg from '../../assets/menu/banner3.jpg'
 import dessertsImg from '../../assets/menu/dessert-bg.jpeg'
+import pizzaImg from '../../assets/menu/pizza-bg.jpg'
+import saladImg from '../../assets/menu/salad-bg.jpg'
+import soupImg from '../../assets/menu/soup-bg.jpg'
 
 const Menu = () => {
     return (
@@ -15,10 +18,16 @@ const Menu = () => {
             </Helmet>
             <div className='min-h-screen'>
                 {/* main section */}
-               <MenuSection img={bannerImg} coverHeading={'Our Menu'} coverSubHeading={'would you like to try a dish?'} category='offered' heading={"today's offer"} subHeading={"Don't miss"}></MenuSection>
-               {/* desserts section */}
-               <MenuSection img={dessertsImg} coverHeading={"Desserts"}
-            coverSubHeading={'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} category={'dessert'}></MenuSection>
+                <MenuSection img={bannerImg} coverHeading={'Our Menu'} coverSubHeading={'would you like to try a dish?'} category='offered' heading={"today's offer"} subHeading={"Don't miss"}></MenuSection>
+                {/* desserts section */}
+                <MenuSection img={dessertsImg} coverHeading={"Desserts"}
+                    coverSubHeading={'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} category={'dessert'}></MenuSection>
+                {/* pizza section */}
+                <MenuSection img={pizzaImg} coverHeading={"pizza"} coverSubHeading={'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} category={'pizza'}></MenuSection>
+                {/* SIlad section */}
+                <MenuSection img={saladImg} coverHeading={"salads"} coverSubHeading={'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} category={'salad'}></MenuSection>
+                {/* soups section */}
+                <MenuSection img={soupImg} coverHeading={"soups"} coverSubHeading={'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} category={'soup'}></MenuSection>
             </div>
         </div>
     );
