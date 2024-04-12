eb8b0eea63b98561525ad7120f5d8d2feb9f7d5e
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-04-12 15:54:19 +0600

    added cart page

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -2,16 +2,16 @@ import {
     useQuery,
 } from '@tanstack/react-query'
 import useAxiosSecure from './useAxiosSecure';
-
+import useAuth from './useAuth'
 const useCart = () => {
+    const {user}=useAuth()
+    // console.log(user?.email)
     const axiosSecure = useAxiosSecure()
     const { data: cart ,refetch } = useQuery({
         queryKey: ['cart'],
         queryFn: async () => {
-            const res = await axiosSecure.get('/addtocart')
-
+            const res = await axiosSecure.get(`/addtocart?email=${user?.email}`)
             return res.data
-
         }
     })
     return [cart,refetch]
--- src/Pages/Dashboard/Cart.jsx
+++ src/Pages/Dashboard/Cart.jsx
@@ -1,9 +1,54 @@
+import { FaDeleteLeft } from 'react-icons/fa6';
 import SectionTitle from '../../Component/Shared/SectionTitle/SectionTitle'
+import useCart from '../../Hooks/useCart';
+import { MdDelete } from "react-icons/md";
 
 const Cart = () => {
+    const [cart] = useCart()
     return (
         <div>
-        <SectionTitle heading={'Wanna add more?'} subHeading={'My Cart'}></SectionTitle>
+            <SectionTitle heading={'Wanna add more?'} subHeading={'My Cart'}></SectionTitle>
+
+
+            <div className="overflow-x-auto mt-10 ">
+                <table className="table table-sm">
+                    <thead>
+                        <tr className='font-medium text-lg text-black'>
+                            <th>#</th>
+                            <th>Item Image</th>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cart?.map((item, index) => {
+                            return (
+
+                                <tr key={item?._id}>
+
+                                    <td>{index + 1}</td>
+                                    <td>{
+                                        <img src={item?.image} alt="" className='w-16 rounded-lg' />
+                                    }</td>
+                                    <td>{item?.name}</td>
+                                    <td>${item?.price}</td>
+                                    <td className=''>
+                                        <button className='bg-red-600 p-3 rounded-md text-white text-xl'><MdDelete></MdDelete></button>
+                                    </td>
+
+                                </tr>
+                            )
+                        })}
+
+                    </tbody>
+
+                </table>
+
+
+
+            </div>
         </div>
     );
 };
