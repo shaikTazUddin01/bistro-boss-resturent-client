6a78822b6bb44495199770bf5935a1d0701bfce6
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-04-12 16:37:17 +0600

    added cart table

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -1,8 +1,11 @@
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import './Dashboard.css'
 import { useEffect, useState } from 'react';
+import useCart from '../Hooks/useCart';
+import { FaCartPlus, FaHome } from 'react-icons/fa';
 const DashBoard = () => {
     const [activeRoute, setActiveRoute] = useState("");
+    const [cart] = useCart()
 
     const location = useLocation()
     const pathNameParts = location.pathname.split('/')
@@ -16,14 +19,17 @@ const DashBoard = () => {
     }, [lastPart])
     return (
         <div className='flex gap-10'>
-            <div className='bg-yellow-600 min-h-screen'>
-                <ul className='px-20 space-y-2 pt-5'>
+            <div className='bg-yellow-600 min-h-screen w-[20%]'>
+                <ul className='px-10 space-y-2 pt-5'>
                     <li><Link to={'/dashboard'}
                         className={activeRoute == 'dashboard' ? 'active' : ''}
-                    >DashBoard</Link></li>
+                    > <FaHome></FaHome> User Home</Link></li>
                     <li><Link to={'/dashboard/cart'}
                         className={activeRoute == 'cart' ? 'active' : ''}
-                    >Cart</Link></li>
+                    ><FaCartPlus></FaCartPlus> Cart ({cart?.length})</Link></li>
+                    <div className='divider'></div>
+                    <li><Link to={'/'}
+                    ><FaHome></FaHome> Home</Link></li>
                 </ul>
             </div>
             <div className='flex-1'>
--- src/Pages/Dashboard/Cart.jsx
+++ src/Pages/Dashboard/Cart.jsx
@@ -2,14 +2,53 @@ import { FaDeleteLeft } from 'react-icons/fa6';
 import SectionTitle from '../../Component/Shared/SectionTitle/SectionTitle'
 import useCart from '../../Hooks/useCart';
 import { MdDelete } from "react-icons/md";
+import useAxiosSecure from '../../Hooks/useAxiosSecure';
+import Swal from 'sweetalert2'
 
 const Cart = () => {
-    const [cart] = useCart()
+    const axiosSecure = useAxiosSecure()
+    const [cart, refetch] = useCart()
+
+const totalPrice=cart?.reduce((previousValue,currentValue)=>previousValue+currentValue?.price,0)
+
+    const handleDelete = (itemId) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/addtocart/${itemId}`)
+                    .then(res => {
+
+                        if (res?.data?.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                            refetch()
+                        }
+                    })
+
+            }
+        });
+
+
+    }
     return (
         <div>
             <SectionTitle heading={'Wanna add more?'} subHeading={'My Cart'}></SectionTitle>
 
-
+            <section className='flex  justify-between items-center py-2 px-5'>
+                <p className='text-2xl font-semibold uppercase'>Total orders: {cart?.length}</p>
+                <p className='text-2xl font-semibold uppercase'>Total price: ${totalPrice}</p>
+                <button className='btn btn-success uppercase'>pay</button>
+            </section>
             <div className="overflow-x-auto mt-10 ">
                 <table className="table table-sm">
                     <thead>
@@ -19,7 +58,7 @@ const Cart = () => {
                             <th>Item Name</th>
                             <th>Price</th>
                             <th>Action</th>
-                            
+
                         </tr>
                     </thead>
                     <tbody>
@@ -35,7 +74,7 @@ const Cart = () => {
                                     <td>{item?.name}</td>
                                     <td>${item?.price}</td>
                                     <td className=''>
-                                        <button className='bg-red-600 p-3 rounded-md text-white text-xl'><MdDelete></MdDelete></button>
+                                        <button className='bg-red-600 p-3 rounded-md text-white text-xl' onClick={() => handleDelete(item?._id)}><MdDelete></MdDelete></button>
                                     </td>
 
                                 </tr>
