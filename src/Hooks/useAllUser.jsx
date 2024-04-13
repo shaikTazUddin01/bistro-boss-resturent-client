d8c25fe1f4212ba0dd1575d6b9d3754aa4182fd9
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-04-13 12:43:24 +0600

    added user delete syatem

--- src/Hooks/useAllUser.jsx
+++ src/Hooks/useAllUser.jsx
@@ -3,14 +3,14 @@ import useAxiosSecure from "./useAxiosSecure";
 
 const useAllUser = () => {
     const axiosSecure = useAxiosSecure()
-    const { data: users ,isPending } = useQuery({
+    const { data: users ,isPending,refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res =await axiosSecure.get('/user')
             return res.data
         }
     })
-    return [users ,isPending]
+    return [users ,isPending ,refetch]
 };
 
 export default useAllUser;
--- src/Pages/Dashboard/AllUser.jsx
+++ src/Pages/Dashboard/AllUser.jsx
@@ -3,19 +3,50 @@ import useAllUser from '../../Hooks/useAllUser';
 import SectionTitle from '../../Component/Shared/SectionTitle/SectionTitle'
 import { MdDelete } from 'react-icons/md';
 import useAxiosSecure from '../../Hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
 const AllUser = () => {
-    const axiosSecure=useAxiosSecure()
-    const [users, isPending] = useAllUser()
+    const axiosSecure = useAxiosSecure()
+    const [users, isPending ,refetch] = useAllUser()
     if (isPending) {
         return <p>Loading...</p>
     }
     const handleDelete = (id) => {
-        console.log(id)
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/user/${id}`)
+                    .then(res => {
+                        if (res?.data?.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                            refetch()
+                        }
+                    })
+                    .catch(err => {
+                        Swal.fire({
+
+                            text: "something is wrong please try again",
+                            icon: "error"
+                        });
+                    })
+            }
+        });
+
 
     }
     return (
         <div>
-            <SectionTitle heading={'Wanna add more?'} subHeading={'My Cart'}></SectionTitle>
+            <SectionTitle heading={'manage all users'} subHeading={'How many??'}></SectionTitle>
 
             <section className='flex  justify-between items-center py-2 px-5'>
                 <p className='text-2xl font-semibold uppercase'>Total Users: {users?.length}</p>
@@ -40,9 +71,7 @@ const AllUser = () => {
                                 <tr key={item?._id}>
 
                                     <td>{index + 1}</td>
-                                    <td>{
-                                        <img src={item?.name} alt="" className='w-16 rounded-lg' />
-                                    }</td>
+                                    <td>{item?.name}</td>
                                     <td>{item?.email}</td>
                                     <td>admin</td>
                                     <td className=''>
